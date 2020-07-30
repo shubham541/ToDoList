9062c54d0c08315dacc666f746905f243116f66d
shubham541 <kumar.shubham541@gmail.com>
2020-07-30 22:19:05 +0530

    Version 1

---
+++ app.js
@@ -0,0 +1,29 @@
+
+const express=require("express");
+const bodyParser=require("body-parser");
+const date=require(__dirname+"/date.js");
+
+console.log(date);
+
+const app=express();
+var items=["Buy Food","Cook Food","Eat Food"];
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+  let day=date.getDate();
+  res.render("list",{kindOfDay:day,newListItems:items});
+});
+
+app.post("/",function(req,res){
+  var item=req.body.newItem;
+  items.push(item);
+
+  res.redirect("/");
+
+});
+app.listen(3000,function(){
+  console.log("Server started at port 3000");
+});
